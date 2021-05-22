a7a1e2f5e2913f1548134b3cca8133a90a3e2d54
Vishrut8898 <vishrut9869@gmail.com>
2021-05-22 19:28:29 +0530

    Second Commit

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index.js')
+const authorRouter = require('./routes/author.js')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,6 +24,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 4000, () => {
     console.log('Server Started')
